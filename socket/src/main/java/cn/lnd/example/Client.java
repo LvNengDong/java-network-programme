8a5268082aea070cc430de8eecfe84e644ffe1d9
nengdong.lv <nengdong.lv@qunar.com>
2023-08-29 20:35:01 +0800

    客户端Socket

--- socket/src/main/java/cn/lnd/example/Client.java
+++ socket/src/main/java/cn/lnd/example/Client.java
@@ -1,9 +1,66 @@
 package cn.lnd.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
+
 /**
  * @Author lnd
  * @Description
  * @Date 2023/8/29 11:18
  */
+@Slf4j
 public class Client {
+    static final String DEFAULT_SERVER_HOST = "127.0.0.1";
+    static final int DEFAULT_SERVER_PORT = 8888;
+    static final String QUIT = "quit";
+    static Socket socket = null;
+
+    public static void main(String[] args) {
+        BufferedWriter writer = null;
+        try {
+            // create & bind & connect
+            log.info("客户端：开始启动");
+            socket = new Socket(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+            log.info("客户端：启动完成，{}:{}", socket.getInetAddress(), socket.getPort());
+            // 创建IO流
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //读取服务器端发送的信息
+            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));//写要发送给服务器端的数据
+            // 等待用户输入信息
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String input = consoleReader.readLine();
+                // 发送消息给服务器
+                writer.write(input + "\n");
+                writer.flush();
+                log.info("客户端：发送消息，msg:{}", input);
+                // 读取服务器返回的消息
+                String msg = reader.readLine();
+                log.info("客户端：接收到服务器返回的消息，msg:{}", msg);
+                // 检查用户是否需要退出
+                if (StringUtils.equalsIgnoreCase(QUIT, msg)) {
+                    log.info("客户端：用户请求退出，开始退出...");
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (null != socket) {
+                try {
+                    /* 为什么不直接关闭Socket而是关闭writer？
+                    *   1、关闭writer时会顺带着关闭Socket
+                    *   2、关闭writer前会执行一遍flush函数，确保缓冲区中没有数据
+                    * */
+                    writer.close();
+                    log.info("客户端：退出完成，关闭Socket");
+                } catch (IOException e) {
+                    log.error("关闭Socket异常", e);
+                }
+            }
+        }
+    }
 }
--- socket/src/main/java/cn/lnd/example/Server.java
+++ socket/src/main/java/cn/lnd/example/Server.java
@@ -15,33 +15,41 @@ import java.net.Socket;
 @Slf4j
 public class Server {
     static final int DEFAULT_PORT = 8888;
-
+    static final String QUIT = "quit";
     public static void main(String[] args) {
 
         ServerSocket serverSocket = null;
         try {
             // bind
             serverSocket = new ServerSocket(DEFAULT_PORT);
-            log.info("启动服务器，监听端口：" + DEFAULT_PORT);
+            log.info("服务器：启动成功，{}:{}", serverSocket.getInetAddress(), serverSocket.getLocalPort());
 
             while (true) {
                 // accept
                 // 1、阻塞式调用  2、当有客户端请求连接时，返回一个socket对象，作为服务器与客户端进行数据交换的端点
                 // 等待客户端连接
                 Socket socket = serverSocket.accept();
-                log.info("客户端[" + socket.getInetAddress() + ":" + socket.getPort() + "]连接成功");
+                log.info("服务器：获取客户端Socket成功，{}:{}", socket.getInetAddress(), socket.getLocalPort());
 
                 // I/O：处理输入输出流
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //取出客户端端点的数据流
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // 向客户端端点写的数据流
 
                 // 读取客户端发送的消息
-                String msg = reader.readLine();
-                if (StringUtils.isNotEmpty(msg)) { //如果在读取数据期间客户端Socket关闭了，那么服务器读到的数据就是null
-                    log.info("客户端[" + socket.getInetAddress() + ":" + socket.getPort() + "]>>" + msg);
+                String msg = null;
+                while ((msg = reader.readLine()) != null) { //如果在读取数据期间客户端Socket关闭了，那么服务器读到的数据就是null
+                    log.info("服务器：获取客户端请求成功，{}:{}，msg:{}", socket.getInetAddress(), socket.getLocalPort(), msg);
                     // 回复客户端消息
                     writer.write("服务器：" + msg + "\n");
                     writer.flush();
+
+                    // 检查客户端是否退出
+                    if (StringUtils.equalsIgnoreCase(QUIT, msg)) {
+                        log.info("服务器：客户端请求断开连接，{}:{}", socket.getInetAddress(), socket.getPort());
+                        break;
+                        /* 实际上，即便不在这里手动退出循环，关闭ServerSocket，如果客户端主动断开了Socket，在while循环进行到下一个循环的时候，
+                        * 也会由于 reader.readLine() 读不到任何内容而导致跳出循环，关闭ServerSocket */
+                    }
                 }
             }
         } catch (Exception e) {
@@ -50,6 +58,7 @@ public class Server {
             if (serverSocket != null) {
                 try {
                     serverSocket.close();
+                    log.info("服务器：关闭ServerSocket");
                 } catch (IOException e) {
                     log.error("服务器关闭异常", e);
                 }
